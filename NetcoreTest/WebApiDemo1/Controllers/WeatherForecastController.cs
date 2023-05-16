859186137546586134ff998216798605a86a29e4
yangfz-lg <2609688892@qq.com>
2023-05-17 00:25:32 +0800

    05-17

--- NetcoreTest/WebApiDemo1/Controllers/WeatherForecastController.cs
+++ NetcoreTest/WebApiDemo1/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApiDemo1.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("test/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
